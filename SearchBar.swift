6aecd1241f5f483792f3487cc457434f1899b6c5
Max M <67687590+rlmtm@users.noreply.github.com>
2021-11-26 16:27:32 +0100

    Add files via upload

--- SearchBar.swift
+++ SearchBar.swift
@@ -6,43 +6,43 @@
 //
 
 import SwiftUI
- 
+
 struct SearchBar: View {
-    @Binding var text: String
+    @State var SearchString: String
  
     @State private var isEditing = false
  
     var body: some View {
         HStack {
  
-            TextField("Search ...", text: $text)
+            TextField("Search ...", SearchString: $SearchString)
                 .padding(7)
                 .padding(.horizontal, 25)
                 .background(Color(.systemGray6))
                 .cornerRadius(8)
                 .padding(.horizontal, 10)
                 .onTapGesture {
+                
                     self.isEditing = true
                 }
  
             if isEditing {
                 Button(action: {
                     self.isEditing = false
-                    self.text = ""
+                    self.SearchString = ""
  
                 }) {
                     Text("Cancel")
                 }
                 .padding(.trailing, 10)
                 .transition(.move(edge: .trailing))
-                .withAnimation(<#T##SwiftUI.Animation?#>, <#T##() -> Result#>)
             }
         }
     }
 }
 
-struct SearchBar_Previews: PreviewProvider {
+/*struct SearchBar: PreviewProvider {
     static var previews: some View {
         SearchBar()
     }
-}
+}*/
